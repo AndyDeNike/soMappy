ec7dffbd8de3e0402e10c24036ebe53e93e2da48
Andydenike <denikeandrew@gmail.com>
2019-05-22 16:51:40 -0700

    Updated ViewController with GMSMapView instance

--- soMappy/ViewController.swift
+++ soMappy/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    // You don't need to modify the default init(nibName:bundle:) method.
+    
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 37.33, longitude: -121.87, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 37.33, longitude: -121.87)
+        marker.title = "The Place"
+        marker.snippet = "USA"
+        marker.map = mapView
     }
-
-
 }
-
